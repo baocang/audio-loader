758617cbf5ef9fc0d4a0c7bd05a44ed8a1a78428
Baocang Nie <baocang.nie@icloud.com>
2021-06-24 23:33:03 +0800

    fix test & bump deps

--- test/node-test.js
+++ test/node-test.js
@@ -4,7 +4,7 @@ var isBuffer = require('is-audio-buffer')
 var load = require('..')
 var wavBuffer = require('audio-lena/wav')
 var mp3Buffer = require('audio-lena/mp3')
-var rawBuffer = require('audio-lena/buffer')
+var rawBuffer = require('audio-lena/raw-base64')
 var t = require('tape')
 var path = require('path')
 
@@ -15,32 +15,68 @@ function testBuffer (buffer) {
 }
 
 t('load wav buffer', function (t) {
-  load(wavBuffer).then(testBuffer).then(() => t.end(), () => t.fail())
+  load(wavBuffer).then(testBuffer).then((buffer) => {
+    t.ok(buffer);
+    t.end();
+  }, () => {
+    t.fail();
+  })
 })
 t('load mp3 buffer', function (t) {
-  load(mp3Buffer).then(testBuffer).then(() => t.end(), () => t.fail())
+  load(mp3Buffer).then(testBuffer).then((buffer) => {
+    t.ok(buffer);
+    t.end();
+  }, () => {
+    t.fail();
+  })
 })
 t('load wav files', function (t) {
-  load('./example/samples/maeclave.wav').then(testBuffer).then(() => t.end(), () => t.fail())
+  load('./example/samples/maeclave.wav').then(testBuffer).then((buffer) => {
+    t.ok(buffer);
+    t.end();
+  }, () => {
+    t.fail();
+  });
 })
 t('should throw error on undecodable data', function (t) {
   load(rawBuffer).then(testBuffer => {
     t.fail()
   }, e => {
     t.ok(e)
-    t.end()
+    t.end();
   })
 })
 t('load mp3 files', function (t) {
-  load('./example/samples/train.mp3').then(testBuffer).then(() => t.end(), () => t.fail())
+  load('./example/samples/train.mp3').then(testBuffer).then((buffer) => {
+    t.ok(buffer);
+    t.end();
+  }, () => {
+    t.fail();
+  })
 })
-t('load m4a files', function (t) {
-  load('./example/samples/bassnote.m4a').then(testBuffer).then(() => t.end(), () => t.fail())
-})
+// m4a nolonger supported
+// t('load m4a files', function (t) {
+//   load('./example/samples/bassnote.m4a').then(testBuffer).then((buffer) => {
+//     t.ok(buffer);
+//     t.end();
+//   }, (err) => {
+//     t.fail();
+//   })
+// })
 t('load absolute paths', function (t) {
-  load(path.resolve('./example/samples/train.mp3')).then(testBuffer).then(() => t.end(), () => t.fail())
+  load(path.resolve('./example/samples/train.mp3')).then(testBuffer).then((buffer) => {
+    t.ok(buffer);
+    t.end();
+  }, () => {
+    t.fail();
+  })
 })
 t('load remote files', function (t) {
   load('https://ia802508.us.archive.org/5/items/testmp3testfile/mpthreetest.mp3')
-  .then(testBuffer).then(() => t.end(), (e) => t.fail(e))
+  .then(testBuffer).then((buffer) => {
+    t.ok(buffer);
+    t.end();
+  }, (e) => {
+    t.fail(e);
+  })
 })
